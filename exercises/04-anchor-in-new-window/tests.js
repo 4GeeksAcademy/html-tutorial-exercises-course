0bcaa1290ce4fe2a33623b7dad5cda32ed9cb985
David Hay <hay.david@hotmail.com>
2019-07-26 19:17:28 +0000

    exercise #4 test done

--- exercises/04-anchor-in-new-window/tests.js
+++ exercises/04-anchor-in-new-window/tests.js
@@ -4,25 +4,13 @@ const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
 jest.dontMock('fs');
 
-describe('ol should exist', function () {
+describe('<a> tag should open in new window', function () {
     beforeEach(() => { document.documentElement.innerHTML = html.toString(); });
     afterEach(() => { jest.resetModules(); });
 
 
- it('ol exists', function () {
-        expect(document.querySelector("ol")).toBeTruthy()
+ it('<a> new window', function () {
+        expect(document.querySelector("a").target).toBeTruthy()
     })
-    it('li exists', function () {
-        expect(document.querySelectorAll("li").length).toBe(6)
-    })
-
-       it("innerHTML exist", function(){
-
-        for(let i=0; i<document.querySelectorAll("li").length;i++){
-             expect(document.querySelectorAll("li")[i].innerHTML).not.toBe("")
-        }
-    })
-
-
 
 });
