af90b1c07bf7532ab22f1a32c34de0b922668e5b
David Hay <hay.david@hotmail.com>
2019-08-02 18:32:40 +0000

    update

--- exercises/11-image-with-text/tests.js
+++ exercises/11-image-with-text/tests.js
@@ -1,13 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
-import React from 'react'
+
+
 
 jest.dontMock('fs');
 
-describe('Replicate Nested Tag Example', function () {
+
+describe('image with text', function () {
     beforeEach(() => { document.documentElement.innerHTML = html.toString(); });
     afterEach(() => { jest.resetModules(); });
+
+
+
+ it('order of HTML tag', function () {
+ for(let i=0;i<document.querySelectorAll("table tr td img").length;i++){
+            if (i===0){
+        expect(document.querySelectorAll("table tr td img")[i].tagName).toStrictEqual(document.querySelector("table").tagName)
+            }
+         if(i===1){
+        expect(document.querySelectorAll("table tr td img")[i].tagName).toStrictEqual(document.querySelector("tr").tagName)
+       }
+           if(i===2){
+        expect(document.querySelectorAll("table tr td img")[i].tagName).toStrictEqual(document.querySelector("td").tagName)
+       }
+ }
+    })
 
 
  it('<table> tag exists', function () {
